cf3b859449876df67d36efa4338557cab1314c69
Shan Wu <peishanwu@alum.calarts.edu>
2018-08-11 14:44:24 -0700

    Fixed formatting

--- Project03_Loveometer.ino
+++ Project03_Loveometer.ino
@@ -1,1 +1,49 @@
-/**/const int sensorPin = A0;const float baselineTemp= 32.0;void setup() {    Serial.begin(9600);    for(int pinNumber = 2; pinNumber<5; pinNumber++){      pinMode(pinNumber,OUTPUT);      digitalWrite(pinNumber, LOW);    }}void loop() {    int sensorVal = analogRead(sensorPin);    Serial.print("Sensor Value:");    Serial.print(sensorVal);        float voltage = (sensorVal/1024.0)*5.0;    Serial.print(", Voltes:");    Serial.print(voltage);    Serial.print(", degrees C:");    float temperature = (voltage - .5)*100;    Serial.print(temperature);        if(temperature < baselineTemp){      digitalWrite(2,LOW);      digitalWrite(3,LOW);      digitalWrite(4,LOW);    }        else if (temperature >= baselineTemp+1 && temperature < baselineTemp+2 ){      digitalWrite(2,HIGH);      digitalWrite(3,LOW);      digitalWrite(4,LOW);    }     else if (temperature >= baselineTemp+2 && temperature < baselineTemp+3 ){      digitalWrite(2,HIGH);      digitalWrite(3,HIGH);      digitalWrite(4,LOW);    }    else if (temperature >= baselineTemp+3){      digitalWrite(2,HIGH);      digitalWrite(3,HIGH);      digitalWrite(4,HIGH);    }    delay(1);}
+/*
+
+*/
+const int sensorPin = A0;
+const float baselineTemp= 32.0;
+
+void setup() {
+    Serial.begin(9600);
+    for(int pinNumber = 2; pinNumber<5; pinNumber++){
+      pinMode(pinNumber,OUTPUT);
+      digitalWrite(pinNumber, LOW);
+    }
+}
+
+void loop() {
+    int sensorVal = analogRead(sensorPin);
+    Serial.print("Sensor Value:");
+    Serial.print(sensorVal);
+
+    float voltage = (sensorVal/1024.0)*5.0;
+    Serial.print(", Voltes:");
+    Serial.print(voltage);
+    Serial.print(", degrees C:");
+    float temperature = (voltage - .5)*100;
+    Serial.print(temperature);
+
+    if(temperature < baselineTemp){
+      digitalWrite(2,LOW);
+      digitalWrite(3,LOW);
+      digitalWrite(4,LOW);
+    }
+
+    else if (temperature >= baselineTemp+1 && temperature < baselineTemp+2 ){
+      digitalWrite(2,HIGH);
+      digitalWrite(3,LOW);
+      digitalWrite(4,LOW);
+    }
+     else if (temperature >= baselineTemp+2 && temperature < baselineTemp+3 ){
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(4,LOW);
+    }
+    else if (temperature >= baselineTemp+3){
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(4,HIGH);
+    }
+    delay(1);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+A bunch of arduino projects, all from the official arduino starter kit
